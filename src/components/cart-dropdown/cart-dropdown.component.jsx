03e0da498b2ac74e373dee92e46eb2d2dbe5b072
Shafaat Karim <eduntechsolutions@gmail.com>
2022-12-18 16:15:30 +0530

    created checkout page along with checkout items and checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { CartContext } from "../../contexts/cart.context";
 
@@ -8,6 +9,13 @@ import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+
+  const navigate = useNavigate();
+
+  const gotoCheckoutHandler = () => {
+    navigate("/checkout");
+  }
+
   const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
@@ -16,7 +24,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>Go to Checkout</Button>
+      <Button type="button" onClick={gotoCheckoutHandler}>Go to Checkout</Button>
     </div>
   );
 };
